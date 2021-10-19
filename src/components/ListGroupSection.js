b27b95828cb75f6efe9c680908649f277acc6138
Édio Cerati Neto <edioceratineto@gmail.com>
2021-10-19 08:55:14 -0300

    Remove unused dependencies from ListGroupSection

--- src/components/ListGroupSection.js
+++ src/components/ListGroupSection.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import ListGroup from 'react-bootstrap/ListGroup';
