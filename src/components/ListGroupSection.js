e4bc73185dd48ff35efcf0dd669d8e16525690a9
Édio Cerati Neto <edioceratineto@gmail.com>
2021-11-17 08:32:47 -0300

    Change create playlist button to be more compact

--- src/components/ListGroupSection.js
+++ src/components/ListGroupSection.js
@@ -100,10 +100,7 @@ export default function ListGroupSection({ title, items, keys, routes, songData,
         :
         <div className="d-grid gap-2">
           <Button className="mb-2" variant="success" onClick={() => handlePlaylistCreate()}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-            </svg>
-            <span>&nbsp;New Playlist</span>
+            + Playlist
           </Button>
         </div >
       }
